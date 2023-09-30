3270ced35cda408ea862b79005a0e187ca563436
Grant Heaslip <grant@asl19.org>
2023-09-30 12:44:12 -0400

    Alphabetically order index exports

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,19 @@
  * @packageDocumentation
  */
 
-export { default as Styles } from "./Styles";
-export { default as insetInlineEnd } from "./insetInlineEnd";
-export { default as insetInlineStart } from "./insetInlineStart";
-export { default as marginBlock } from "./marginBlock";
-export { default as marginInline } from "./marginInline";
-export { default as paddingBlock } from "./paddingBlock";
+export { default as devLabel } from "./devLabel";
 export { default as gridContainer } from "./gridContainer";
-export { default as lineClampedText } from "./lineClampedText";
-export { default as transitionDurationWithPrefersReducedMotion } from "./transitionDurationWithPrefersReducedMotion";
-export { default as hoverStyles } from "./hoverStyles";
 export { default as hiddenWhenJs } from "./hiddenWhenJs";
 export { default as hiddenWhenNoJs } from "./hiddenWhenNoJs";
 export { default as hiddenWhenPointerCoarseOrNone } from "./hiddenWhenPointerCoarseOrNone";
 export { default as hiddenWhenPointerFine } from "./hiddenWhenPointerFine";
+export { default as hoverStyles } from "./hoverStyles";
+export { default as insetInlineEnd } from "./insetInlineEnd";
+export { default as insetInlineStart } from "./insetInlineStart";
 export { default as invisible } from "./invisible";
-export { default as devLabel } from "./devLabel";
+export { default as lineClampedText } from "./lineClampedText";
+export { default as marginBlock } from "./marginBlock";
+export { default as marginInline } from "./marginInline";
+export { default as paddingBlock } from "./paddingBlock";
+export { default as transitionDurationWithPrefersReducedMotion } from "./transitionDurationWithPrefersReducedMotion";
+export { default as Styles } from "./Styles";
