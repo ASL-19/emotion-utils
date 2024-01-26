d6fa55e48d1ec75d7deea2bdbcf9049de0e34885
Grant Heaslip <grant@asl19.org>
2024-01-25 21:44:50 -0500

    In lineClampedText add optional height argument (with new "auto" value) and lineHeight style (via internal project)

--- src/lineClampedText.ts
+++ src/lineClampedText.ts
@@ -4,25 +4,46 @@ import { css, SerializedStyles } from "@emotion/react";
  * Clamp with ellipsis when text extends past the provided number of lines.
  *
  * @remarks
- * The box will always have a height equivalent to `lineCount` * `lineHeight` *
- * `fontSize`. This is necessary to ensure the overflow is cut off at the
- * correct vertical position.
+ * By default the box will have a fixed height equivalent calculated using
+ * `fontSize`, `lineCount`, and `lineHeight`. This can be overridden by passing
+ * `height: "auto"` (more details in `height` argument docstring).
  *
  * @public
  */
 const lineClampedText = ({
   fontSize,
+  height = "fixed",
   lineCount,
   lineHeight,
 }: {
   fontSize: string;
+  /**
+   * Values:
+   *
+   * - `"fixed"` (default): The resolved `height` will be calculated based on
+   *   `fontSize`, `lineCount`, and `lineHeight`.
+   *
+   *   The element will always take up the same amount of vertical space, which
+   *   is good for layouts where vertical alignment is important.
+   *
+   * - `"auto"`: The resolved `height` will be `auto`.
+   *
+   *   The element’s height will be fully dynamic (based on its content), but
+   *   still constrained to `lineCount`. This is good for layouts where vertical
+   *   rhythm is more important than vertical alignment.
+   */
+  height?: "auto" | "fixed";
   lineCount: number;
   lineHeight: number;
 }): SerializedStyles =>
   css({
     display: "-webkit-box",
     fontSize,
-    height: `calc(${lineCount * lineHeight} * ${fontSize})`,
+    height:
+      height === "auto"
+        ? "auto"
+        : `calc(${lineCount * lineHeight} * ${fontSize})`,
+    lineHeight,
     overflow: "hidden",
     textOverflow: "ellipsis",
     WebkitBoxOrient: "vertical",
